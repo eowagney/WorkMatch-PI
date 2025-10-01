c21c10372ab48fca29a473d2574c311cf0dabc03
eowagney <wagnergomes9a@gmail.com>
2025-09-30 22:36:58 -0300

    Criação do Projeto PI

---
+++ backend/src/main/java/com/workmatch/App.java
@@ -0,0 +1,13 @@
+package com.workmatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
